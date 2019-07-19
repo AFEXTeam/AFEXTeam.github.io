edef07df069269335ee018cdafdbd531a596f45f
CrownHuang <buaahg@163.com>
2019-07-19 16:05:02 +0800

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,7 @@ var menulist = [
   {
     menuGroupName: "产品",
     list: [
-      { name: "全渠道前端开发", url: "https://github.com/AFEXTeam/abc-pc" },
+      { name: "全渠道前端开发", url: "https://afexteam.github.io/abc-pc/" },
       { name: "移动端基座开发", url: "https://abc.js.org" },
       { name: "企业移动管理平台", url: "https://afexteam.github.io/aemm" },
       { name: "企业小程序平台", url: "https://aemp.js.org" }
